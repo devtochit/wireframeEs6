461576febc4f559ca5256274793f4ee39b8f336a
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-05-30 14:49:55 -0600

    updated the index.js file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import bookData from './modules/Books';
+import Books from './modules/Books.js';
 import { populateStorage, populateNewForm } from './modules/local_storage.js';
 import dynamicLoad from './modules/list_books.js';
 import { inputTitle, inputAuthor } from './modules/user_interface.js';
 
-const awesome = new bookData();
+const awesome = new Books();
 
 if (!localStorage.getItem('data')) {
   populateStorage(inputTitle.value, inputAuthor.value);
